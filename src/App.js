a86bb350c5ac1d7d734fbe78d70b03d058b265b1
Daniel Gamboa <danielgamboa10@gmail.com>
2021-01-29 09:08:40 -0600

    Install styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
+    <header>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <nav className="site-nav">
+        <ul>
+          <li>Home</li>
+          <li>Help</li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 export default App;
