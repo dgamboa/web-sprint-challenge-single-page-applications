be42209b7f0a64c6fb8cc97dd0c121159421f74f
Daniel Gamboa <danielgamboa10@gmail.com>
2021-01-29 09:28:35 -0600

    Style the header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import React from "react";
 import { Route } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  border: 1px solid black; /* Delete */
+  display: flex;
+  justify-content: space-between;
+  height: 8rem;
+  h1 {
+    border: 1px solid black; /* Delete */
+    color: red;
+    font-size: 2.5rem;
+    margin: auto 2rem;
+    text-transform: uppercase;
+  }
+  nav {
+    border: 1px solid black; /* Delete */
+    display: flex;
+    align-items: center;
+    ul {
+      border: 1px solid red; /* Delete */
+      display: flex;
+      justify-content: center;
+      padding: 0;
+      width: 23rem;
+      li {
+        border: 1px solid #333;
+        display: inline-block;
+        list-style: none;
+        flex-grow: 1;
+        text-align: center;
+        line-height: 3rem;
+        font-size: 1.5rem;
+        &:hover {
+          background-color: red;
+          color: white;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+`
 
 const App = () => {
   return (
-    <header>
+    <StyledHeader>
       <h1>Lambda Eats</h1>
       <nav className="site-nav">
         <ul>
@@ -11,7 +52,7 @@ const App = () => {
           <li>Help</li>
         </ul>
       </nav>
-    </header>
+    </StyledHeader>
   );
 };
 export default App;
